0ae148041bb14f087509e1ecb335d9645b270a64
Martin McKeaveney <martinmckeaveney@gmail.com>
2018-06-24 18:08:57 +0100

    chapter 5

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  #get 'static_pages/home' # Will be called static_pages_home_url
+  get '/help',    to: 'static_pages#help' # shorter, named routes 
+  get '/about',   to: 'static_pages#about' # , as: 'foo' will let you use a different name (foo_url, foo_path)
+  get '/contact', to: 'static_pages#contact'
+  get '/signup',  to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
